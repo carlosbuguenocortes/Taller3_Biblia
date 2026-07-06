ded9cbf51240fa1f620a4b4c214b0dcee08b5e40
carlosbuguenocortes <carlos.bugueno@alumnos.ucn.cl>
2026-07-06 19:25:49 -0400

    Primer dashboard funcional

--- api/main.py
+++ api/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from services.data_loader import load_bible
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import PCA
+from collections import Counter
+import re
+import random
+
 
 app = FastAPI()
 
@@ -13,6 +20,24 @@ def inicio():
         "columnas": list(df.columns)
     }
 
+def get_testament(book):
+
+    old_testament = [
+        "Génesis","Éxodo","Levítico","Números","Deuteronomio",
+        "Josué","Jueces","Rut","1 Samuel","2 Samuel",
+        "1 Reyes","2 Reyes","1 Crónicas","2 Crónicas",
+        "Esdras","Nehemías","Esther","Job","Salmos",
+        "Proverbios","Ecclesiastés","Canción de canciones",
+        "Isaías","Jeremías","Lamentaciones","Ezequiel",
+        "Daniel","Oseas","Joel","Amós","Abdías",
+        "Jonás","Miqueas","Nahum","Habacuc",
+        "Sofonías","Haggeo","Zacarías","Malaquías"
+    ]
+
+    if book in old_testament:
+        return "Antiguo Testamento"
+
+    return "Nuevo Testamento"
 
 @app.get("/dashboard")
 def dashboard():
@@ -65,3 +90,413 @@ def get_book(book_name: str):
     return result.to_dict(
         orient="records"
     )
+
+@app.get("/top-books")
+def top_books():
+
+    df = load_bible()
+
+    result = (
+        df.groupby("Book Name")
+        .size()
+        .sort_values(ascending=False)
+        .head(10)
+    )
+
+    return result.to_dict()
+#busqueda de versiculos por texto
+@app.get("/search")
+def search(text: str):
+
+    df = load_bible()
+
+    result = df[
+        df["Text"].str.contains(
+            text,
+            case=False,
+            na=False
+        )
+    ]
+
+    return result.head(50).to_dict(
+        orient="records"
+    )
+
+@app.get("/random-verse")
+def random_verse():
+
+    df = load_bible()
+
+    verse = df.sample(1)
+
+    return verse.to_dict(
+        orient="records"
+    )[0]
+
+@app.get("/book-stats/{book_name}")
+def book_stats(book_name: str):
+
+    df = load_bible()
+
+    book = df[df["Book Name"] == book_name]
+
+    return {
+        "versiculos": len(book),
+        "capitulos": int(book["Chapter"].max())
+    }
+
+@app.get("/search-semantic")
+def search_semantic(text: str):
+
+    df = load_bible()
+
+    verses = df["Text"].fillna("").tolist()
+
+    vectorizer = TfidfVectorizer()
+
+    tfidf_matrix = vectorizer.fit_transform(
+        verses
+    )
+
+    query_vector = vectorizer.transform(
+        [text]
+    )
+
+    similarities = cosine_similarity(
+        query_vector,
+        tfidf_matrix
+    ).flatten()
+
+    top_indices = similarities.argsort()[-10:][::-1]
+
+    results = []
+
+    for idx in top_indices:
+
+        results.append({
+            "book": df.iloc[idx]["Book Name"],
+            "chapter": int(df.iloc[idx]["Chapter"]),
+            "verse": int(df.iloc[idx]["Verse"]),
+            "text": df.iloc[idx]["Text"],
+            "score": float(similarities[idx])
+        })
+
+    return results
+
+@app.get("/pca-2d")
+def pca_2d():
+
+    df = load_bible()
+
+    sample = df.sample(
+        min(500, len(df)),
+        random_state=42
+    )
+
+    vectorizer = TfidfVectorizer(
+        max_features=500
+    )
+
+    X = vectorizer.fit_transform(
+        sample["Text"]
+    )
+
+    pca = PCA(
+        n_components=2
+    )
+
+    coords = pca.fit_transform(
+        X.toarray()
+    )
+
+    result = []
+
+    for i, row in enumerate(sample.itertuples()):
+
+        result.append({
+            "x": float(coords[i][0]),
+            "y": float(coords[i][1]),
+            "book": row._2,
+            "text": row.Text[:100]
+        })
+
+    return result
+
+@app.get("/pca-3d")
+def pca_3d():
+
+    df = load_bible()
+
+    sample = df.sample(
+        min(500, len(df)),
+        random_state=42
+    )
+
+    vectorizer = TfidfVectorizer(
+        max_features=500
+    )
+
+    X = vectorizer.fit_transform(
+        sample["Text"]
+    )
+
+    pca = PCA(
+        n_components=3
+    )
+
+    coords = pca.fit_transform(
+        X.toarray()
+    )
+
+    result = []
+
+    for i, row in enumerate(sample.itertuples()):
+
+        result.append({
+            "x": float(coords[i][0]),
+            "y": float(coords[i][1]),
+            "z": float(coords[i][2]),
+            "book": row._2,
+            "text": row.Text[:100]
+        })
+
+    return result
+
+@app.get("/avg-verse-length")
+def avg_verse_length():
+
+    df = load_bible()
+
+    df["length"] = (
+        df["Text"]
+        .astype(str)
+        .str.len()
+    )
+
+    result = (
+        df.groupby("Book Name")["length"]
+        .mean()
+        .sort_values(
+            ascending=False
+        )
+    )
+
+    return result.to_dict()
+
+@app.get("/top-words")
+def top_words():
+
+    df = load_bible()
+
+    text = " ".join(
+        df["Text"].astype(str)
+    ).lower()
+
+    words = re.findall(
+        r"\b\w+\b",
+        text
+    )
+
+    
+    stopwords = {
+        "de","la","el","y","que",
+        "en","a","los","las",
+        "del","por","con","un",
+        "una","se","su","al",
+        "á","no","para","porque",
+        "él","tu","es","lo",
+        "como","mi","sobre",
+        "yo","mas","ellos"
+    }
+
+
+    words = [
+        w for w in words
+        if w not in stopwords
+    ]
+
+    counter = Counter(words)
+
+    return dict(
+        counter.most_common(20)
+    )
+
+@app.get("/filter")
+def filter_data(
+    testament: str = None,
+    book: str = None,
+    chapter: int = None
+):
+
+    df = load_bible()
+
+    df["Testament"] = (
+        df["Book Name"]
+        .apply(get_testament)
+    )
+
+    if testament:
+        df = df[
+            df["Testament"] == testament
+        ]
+
+    if book:
+        df = df[
+            df["Book Name"] == book
+        ]
+
+    if chapter:
+        df = df[
+            df["Chapter"] == chapter
+        ]
+
+    return df.head(100).to_dict(
+        orient="records"
+    )
+
+@app.get("/generate-unigram")
+def generate_unigram(
+    length: int = 20
+):
+
+    df = load_bible()
+
+    words = " ".join(
+        df["Text"]
+    ).split()
+
+    generated = []
+
+    for _ in range(length):
+        generated.append(
+            random.choice(words)
+        )
+
+    return {
+        "text": " ".join(generated)
+    }
+
+@app.get("/generate-bigram")
+def generate_bigram(
+    start: str,
+    length: int = 20
+):
+
+    import random
+
+    df = load_bible()
+
+    words = " ".join(
+        df["Text"].astype(str)
+    ).split()
+
+    pairs = {}
+
+    for i in range(len(words) - 1):
+
+        current = words[i]
+        next_word = words[i + 1]
+
+        if current not in pairs:
+            pairs[current] = []
+
+        pairs[current].append(next_word)
+
+    generated = [start]
+
+    current = start
+
+    for _ in range(length - 1):
+
+        if current not in pairs:
+            break
+
+        current = random.choice(
+            pairs[current]
+        )
+
+        generated.append(current)
+
+    return {
+        "text": " ".join(generated)
+    }
+    
+@app.get("/generate-trigram")
+def generate_trigram(
+    start: str,
+    length: int = 20
+):
+
+    import random
+
+    df = load_bible()
+
+    words = " ".join(
+        df["Text"].astype(str)
+    ).split()
+
+    if len(words) < 3:
+        return {
+            "text": "Corpus demasiado pequeño"
+        }
+
+    triples = {}
+
+    for i in range(len(words) - 2):
+
+        key = (
+            words[i],
+            words[i + 1]
+        )
+
+        next_word = words[i + 2]
+
+        if key not in triples:
+            triples[key] = []
+
+        triples[key].append(
+            next_word
+        )
+
+    # buscar alguna pareja que comience con la palabra inicial
+    start_pair = None
+
+    for key in triples:
+
+        if key[0].lower() == start.lower():
+            start_pair = key
+            break
+
+    if start_pair is None:
+        return {
+            "text": f"No encontré la palabra inicial '{start}'"
+        }
+
+    generated = [
+        start_pair[0],
+        start_pair[1]
+    ]
+
+    current_pair = start_pair
+
+    for _ in range(length - 2):
+
+        if current_pair not in triples:
+            break
+
+        next_word = random.choice(
+            triples[current_pair]
+        )
+
+        generated.append(
+            next_word
+        )
+
+        current_pair = (
+            current_pair[1],
+            next_word
+        )
+
+    return {
+        "text": " ".join(generated)
+    }
--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,23 +1,407 @@
 import streamlit as st
 import requests
+import pandas as pd
+import plotly.express as px
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
-st.title("Dashboard Biblia")
 
+st.title(" Dashboard Biblia")
+
+# 1) Dashboard principal
+st.header("Dashboard Principal")
 data = requests.get(
     "http://127.0.0.1:8000/dashboard"
 ).json()
 
-st.metric(
-    "Versículos",
-    data["total_versiculos"]
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.metric(
+        "Versículos",
+        data["total_versiculos"]
+    )
+
+with col2:
+    st.metric(
+        "Libros",
+        data["total_libros"]
+    )
+
+with col3:
+    st.metric(
+        "Capítulos",
+        data["total_capitulos"]
+    )
+
+
+#filtros 
+st.header("Filtros")
+
+books = requests.get(
+    "http://127.0.0.1:8000/books"
+).json()
+
+selected_book = st.selectbox(
+    "Selecciona un libro",
+    books,
+    key="book_selector"
+)
+testament = st.selectbox(
+    "Testamento",
+    [
+        "",
+        "Antiguo Testamento",
+        "Nuevo Testamento"
+    ]
+)
+chapter = st.number_input(
+    "Capítulo",
+    min_value=1,
+    value=1
+)
+filtered = requests.get(
+    "http://127.0.0.1:8000/filter",
+    params={
+        "testament": testament,
+        "book": selected_book,
+        "chapter": chapter
+    }
+).json()
+
+st.write(
+    f"Resultados del filtro: {len(filtered)}"
+)
+st.divider()
+
+#==========
+# Top libros
+#==========
+books = requests.get(
+    "http://127.0.0.1:8000/top-books"
+).json()
+
+df_books = pd.DataFrame(
+    books.items(),
+    columns=["Libro", "Versiculos"]
 )
 
-st.metric(
-    "Libros",
-    data["total_libros"]
+fig = px.bar(
+    df_books,
+    x="Libro",
+    y="Versiculos",
+    title="Top 10 Libros con Más Versículos"
 )
 
-st.metric(
-    "Capítulos",
-    data["total_capitulos"]
+st.plotly_chart(
+    fig,
+    use_container_width=True
 )
+
+#Top palabras
+st.header("Top 20 Palabras")
+
+words = requests.get(
+    "http://127.0.0.1:8000/top-words"
+).json()
+
+df_words = pd.DataFrame(
+    words.items(),
+    columns=["Palabra", "Frecuencia"]
+)
+
+fig = px.bar(
+    df_words,
+    x="Palabra",
+    y="Frecuencia",
+    title="Palabras Más Frecuentes"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
+
+#longitud promedio de versiculos
+#========
+st.header(
+    "Longitud Promedio de Versículos"
+)
+
+avg = requests.get(
+    "http://127.0.0.1:8000/avg-verse-length"
+).json()
+
+df_avg = pd.DataFrame(
+    avg.items(),
+    columns=[
+        "Libro",
+        "Longitud"
+    ]
+)
+
+fig = px.bar(
+    df_avg.head(15),
+    x="Libro",
+    y="Longitud",
+    title="Top 15 Libros por Longitud Promedio"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
+
+#nube de palabras
+st.header(f"Nube de Palabras - {selected_book}")
+results = requests.get(
+    f"http://127.0.0.1:8000/book/{selected_book}"
+).json()
+
+texto = " ".join(
+    [r["Text"] for r in results]
+)
+
+wordcloud = WordCloud(
+    width=800,
+    height=400,
+    background_color="white"
+).generate(texto)
+
+fig, ax = plt.subplots()
+
+ax.imshow(wordcloud)
+
+ax.axis("off")
+
+st.pyplot(fig)
+
+stats = requests.get(
+    f"http://127.0.0.1:8000/book-stats/{selected_book}"
+).json()
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.metric(
+        "Capítulos",
+        stats["capitulos"]
+    )
+
+with col2:
+    st.metric(
+        "Versículos",
+        stats["versiculos"]
+    )
+
+st.divider()
+
+#explorador de libros
+st.header("Explorador de Libros")
+book_data = requests.get(
+    f"http://127.0.0.1:8000/book/{selected_book}"
+).json()
+
+st.subheader(f"Versículos de {selected_book}")
+st.write(
+    f" Total de versículos: {len(book_data)}"
+)
+
+cantidad = st.slider(
+    "Cantidad de versículos",
+    5,
+    50,
+    10
+)
+
+for verse in book_data[:cantidad]:
+    st.info(
+        f"{verse['Chapter']}:{verse['Verse']} - {verse['Text']}"
+)
+    
+st.divider()    
+#buscador de palabras
+st.header("Buscadores")
+st.subheader("Búsqueda por Palabra")
+query = st.text_input(
+    "Ingrese una palabra"
+)
+
+if query:
+
+    results = requests.get(
+        f"http://127.0.0.1:8000/search?text={query}"
+    ).json()
+
+    st.write(
+        f"Resultados encontrados: {len(results)}"
+    )
+
+    for verse in results:
+
+        st.write(
+            f" {verse['Book Name']} "
+            f"{verse['Chapter']}:{verse['Verse']}"
+        )
+
+        st.write(
+            verse["Text"]
+        )
+
+        st.divider()
+
+#nueva sección de buscador semántico
+st.subheader("Buscador Semántico TF-IDF")
+semantic_query = st.text_input(
+    "Consulta semántica"
+)
+if semantic_query:
+
+    semantic_results = requests.get(
+        f"http://127.0.0.1:8000/search-semantic?text={semantic_query}"
+    ).json()
+
+    for result in semantic_results:
+
+        st.success(
+            f"{result['book']} "
+            f"{result['chapter']}:{result['verse']}"
+        )
+
+        st.write(
+            result["text"]
+        )
+
+        st.write(
+            f"Similitud: {result['score']:.4f}"
+        )
+
+        st.divider()
+
+#=========
+#pca 2d
+st.divider()
+st.header("Visualizador PCA")
+st.subheader("PCA 2D")
+pca_data = requests.get(
+    "http://127.0.0.1:8000/pca-2d"
+).json()
+
+df_pca = pd.DataFrame(
+    pca_data
+)
+
+fig = px.scatter(
+    df_pca,
+    x="x",
+    y="y",
+    color="book",
+    hover_data=["text"],
+    title="PCA 2D de Versículos"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
+
+#pca 3d
+st.subheader("PCA 3D")
+pca3d = requests.get(
+    "http://127.0.0.1:8000/pca-3d"
+).json()
+
+df_pca3d = pd.DataFrame(
+    pca3d
+)
+
+fig3d = px.scatter_3d(
+    df_pca3d,
+    x="x",
+    y="y",
+    z="z",
+    color="book",
+    hover_data=["text"],
+    title="PCA 3D de Versículos"
+)
+
+st.plotly_chart(
+    fig3d,
+    use_container_width=True
+)
+
+st.divider()
+#generador de versículos
+st.header("Generador de Versículos")
+model = st.selectbox(
+    "Modelo",
+    [
+        "unigram",
+        "bigram",
+        "trigram"
+    ],
+    key="model_selector"
+)
+
+start_word = st.text_input(
+    "Palabra inicial",
+    value="Dios",
+    key="start_word"
+)
+
+max_length = st.slider(
+    "Largo máximo",
+    5,
+    50,
+    20,
+    key="length_slider"
+)
+
+if st.button(
+    "Generar",
+    key="generate_button"
+):
+
+    if model == "unigram":
+
+        result = requests.get(
+            f"http://127.0.0.1:8000/generate-unigram?length={max_length}"
+        ).json()
+
+    elif model == "bigram":
+
+        result = requests.get(
+            f"http://127.0.0.1:8000/generate-bigram?start={start_word}&length={max_length}"
+        ).json()
+
+    else:
+
+        result = requests.get(
+            f"http://127.0.0.1:8000/generate-trigram?start={start_word}&length={max_length}"
+        ).json()
+
+    st.success(
+        result["text"]
+    )
+
+
+st.divider()
+#versículo aleatorio
+st.header("Versículo Aleatorio")
+
+if st.button("Generar Versículo"):
+
+    verse = requests.get(
+        "http://127.0.0.1:8000/random-verse"
+    ).json()
+
+    st.success(
+        f"{verse['Book Name']} "
+        f"{verse['Chapter']}:{verse['Verse']}"
+    )
+
+    st.write(
+        verse["Text"]
+    )
+
